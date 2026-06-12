c8f8146cf1612e718e1913ef97a988510a2339c7
S DURGA SPURTHI SRI <spurthisridurga@gmail.com>
2026-06-12 18:43:10 +0530

    Day 1 - Python Variables and HTML Basics

---
+++ day1_python.py
@@ -0,0 +1,21 @@
+# Variables
+name = "Spurthi"
+age = 19
+college = "Aditya Women's Degree College"
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+
+# Input and Output
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+
+print("\n----- Student Details -----")
+print("Name:", user_name)
+print("Age:", user_age)
+
+if user_age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
