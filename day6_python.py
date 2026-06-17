d1cddec63d8f7c9cdba2dcfe619ea123fe262a67
S DURGA SPURTHI SRI <spurthisridurga@gmail.com>
2026-06-17 19:56:49 +0530

    Day 6 - Dictionaries and CSS Basics

---
+++ day6_python.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Spurthi",
+    "course": "B.Sc Computer Science",
+    "year": "Final Year"
+}
+
+print("Student Details")
+print("Name:", student["name"])
+print("Course:", student["course"])
+print("Year:", student["year"])
