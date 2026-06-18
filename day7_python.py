999c409359545690bd4f7d5c256812616c43140d
S DURGA SPURTHI SRI <spurthisridurga@gmail.com>
2026-06-18 13:09:06 +0530

    Day 7 - Tuples and HTML Links

---
+++ day7_python.py
@@ -0,0 +1,10 @@
+# Day 7 - Tuples
+
+subjects = ("Python", "HTML", "CSS", "Git")
+
+print("Subjects:")
+for subject in subjects:
+    print(subject)
+
+print("\nFirst Subject:", subjects[0])
+print("Total Subjects:", len(subjects))
