8d29fcb0591e5b18aeffa8e7b4745f8147d7bfb1
S DURGA SPURTHI SRI <spurthisridurga@gmail.com>
2026-06-19 20:02:36 +0530

    Day 8 - Sets and HTML Multimedia

---
+++ day8_python.py
@@ -0,0 +1,23 @@
+# Day 8 - Sets
+
+skills = {"Python", "HTML", "CSS", "Python", "Git"}
+
+print("Skills:")
+for skill in skills:
+    print(skill)
+
+print("Total Unique Skills:", len(skills))
+
+
+
+
+
+languages = {"Python", "Java", "C"}
+
+languages.add("SQL")
+
+print(languages)
+
+languages.remove("Java")
+
+print(languages)
