89dc30ae1f6cba700b988ab57a6ddea131dd718a
S DURGA SPURTHI SRI <spurthisridurga@gmail.com>
2026-06-22 19:31:56 +0530

    Day 11 - Modules and Flexbox

---
+++ day11_python.py
@@ -0,0 +1,27 @@
+def greet(name):
+    print("Hello,", name)
+
+def square(num):
+    return num * num
+
+
+
+
+
+import mymodule
+
+mymodule.greet("Spurthi")
+
+result = mymodule.square(5)
+
+print("Square =", result)
+
+
+
+
+
+
+import math
+
+print("Square Root:", math.sqrt(64))
+print("Power:", math.pow(2, 5))
